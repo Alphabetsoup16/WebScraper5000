7061be23a7b20be27424611e39721ccdc4fc712a
LoneHippie <lonehippie.music@gmail.com>
2022-02-20 03:51:39 +0200

    refactored ExtractAllImages and ExtractAllLinks to return predictable data structures or None in the case of no data being present to help with control flow

--- utilities.py
+++ utilities.py
@@ -20,20 +20,31 @@ def GetHeaderInfo(soup: BeautifulSoup):
 
 def ExtractAllImages(soup: BeautifulSoup):
     images = soup.find_all('img')
-    if(len(image) > 0):
-        for image in images:
-            imageAlt = image.get('alt')
-            imageSrc = image.get('src')
-            print(f"Alternative Text: {imageAlt} Source: {imageSrc}")
-    else:
-        print("No images found")
+
+    if(len(image)) == 0: return None
+
+    image_collection = []
+
+    for image in images:
+        img = {
+            "alt": image.get('alt'),
+            "src": image.get('src')
+        }
+        image_collection.append(img)
+    return image_collection
 
 
 def ExtractAllLinks(soup: BeautifulSoup, linkText):
     links = soup.find_all('a', text=f"{linkText}")
-    if(len(links) > 0):
-        for link in links:
-            print(link.get('href'))
+
+    if(len(links) == 0): return None
+
+    link_collection = []
+
+    for link in links:
+        link_collection.append(link.get('href'))
+    return link_collection
+        
 
 # Made previous function modular and reusable for grabbing data from a list of card like html els that can be expected to have the same structure
 def GetNestedPropsList(soup: BeautifulSoup, elType, className, childrenTargets):
