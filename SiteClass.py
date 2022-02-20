daf5cc1d93b16051633ba338b2b4e6e366477b64
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-02-19 22:29:37 -0500

    testing child stuff

--- SiteClass.py
+++ SiteClass.py
@@ -1,1 +1,22 @@
 # Just a place for testing ideas*
+
+from bs4 import BeautifulSoup
+
+# Testing child retrieval
+
+
+def GetNestedPropsList2(soup: BeautifulSoup, parentTarget, childrenTargets):
+
+    parent_element = soup.find_all(class_=parentTarget)
+
+    # data_collection = []
+
+    # for element in el_collection:
+    #     el = {}
+
+    #     for childAttr in childrenTargets:
+    #         el[childAttr] = element.find(class_=childAttr).text.strip()
+
+    #     data_collection.append(el)
+
+    # return data_collection
