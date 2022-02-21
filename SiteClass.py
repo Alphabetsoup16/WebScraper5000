edcd88c37b449c27f3040dffff4e2ef63141fe4c
Spencer <81780117+Alphabetsoup16@users.noreply.github.com>
2022-02-20 22:53:00 -0500

    api branch (#7)
    * initial push for api branch
    
    * fixed
    
    * started
    
    * uploaded example request JSON file for ideas
    
    * starting to create classes
    
    * static parser start
    
    * mmm maybe this will do
    
    * better
    
    * more testing
    
    * moved function out of main to testing file
    
    * moved
    
    * maybe better like this
    
    * add json
    
    Co-authored-by: LoneHippie <lonehippie.music@gmail.com>

--- SiteClass.py
+++ SiteClass.py
@@ -1,6 +1,27 @@
 # Just a place for testing ideas*
 
+from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
+import requests
+
+
+def SpecificElementFinder(soup: BeautifulSoup, element, substring) -> None:
+
+    all_specific_elements = soup.find_all(
+        f"{element}", string=lambda text: f"{substring}" in text.lower()
+    )
+
+    # # Still specific to this particular site...
+    # specific_elements = [
+    #     el.parent.parent.parent for el in all_specific_elements
+    # ]
+
+    # print(f"Found: {len(all_specific_elements)} of those jobs")
+
+    # # Also specific to this site
+    # for job_element in specific_elements:
+    #     link_url = job_element.find_all("a")[1]["href"]
+    #     print(f"Apply here: {link_url}\n")
 
 # Testing child retrieval
 
@@ -11,3 +32,30 @@ def GetNestedPropsList2(soup: BeautifulSoup):
 
     for child in parent_element:
         print(child.get_text().strip())
+
+# Testing find/findall for any element
+
+
+def GetElementByAttribute(soup: BeautifulSoup, AttrType, Attrs):
+    specific_element = soup.find_all(AttrType, Attrs)
+    for attr in specific_element:
+        print(attr.get_text().strip())
+
+
+# Testing classes
+# trying to think of more general abstract methods vs just putting methods in the sub classes
+
+
+class Parser(ABC):
+    @abstractmethod
+    def GetWebPageHtmlData(self):
+        '''Gets the html from webpage'''
+
+
+class StaticParser(Parser):
+    def __init__(self, url):
+        self.url = url
+
+    def GetWebPageHtmlData(self):
+        html = requests.get(self.url)
+        return BeautifulSoup(html.content, "html.parser")
--- app.py
+++ app.py
@@ -1,25 +1,7 @@
 from bs4 import BeautifulSoup
-from SiteClass import GetNestedPropsList2
+import json
+from SiteClass import GetElementByAttribute, GetNestedPropsList2
 from utilities import GetMeTheSoup, GetNestedPropsList, ExtractAllImages, ExtractAllLinks, GetHeaderInfo
-
-
-def SpecificElementFinder(soup: BeautifulSoup, element, substring) -> None:
-
-    all_specific_elements = soup.find_all(
-        f"{element}", string=lambda text: f"{substring}" in text.lower()
-    )
-
-    # # Still specific to this particular site...
-    # specific_elements = [
-    #     el.parent.parent.parent for el in all_specific_elements
-    # ]
-
-    # print(f"Found: {len(all_specific_elements)} of those jobs")
-
-    # # Also specific to this site
-    # for job_element in specific_elements:
-    #     link_url = job_element.find_all("a")[1]["href"]
-    #     print(f"Apply here: {link_url}\n")
 
 
 def main() -> None:
@@ -37,9 +19,14 @@ def main() -> None:
         "links": ExtractAllLinks(soup, "Apply")
     }
 
-    # GetNestedPropsList2(soup)
-    # print(extractedSoup)
+    # turns dict into Json...
+    extractedJSON = json.dumps(extractedSoup)
+    print(extractedJSON)
 
+    # testing this function, the idea is that
+    # We can find/find_all by "attributes" ex: {"id" : "blah"}
+    # Makes it more customizable than using class_ = or id =
+    print(GetElementByAttribute(soup, "h2", {"class": "title"}))
     # GetHeaderInfo(soup)
 
     # TODO: Need to make generic
