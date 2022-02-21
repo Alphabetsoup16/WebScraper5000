a054267dfe314efe94569d7db88918b55ffabb65
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-02-20 19:17:24 -0500

    moved

--- SiteClass.py
+++ SiteClass.py
@@ -4,6 +4,25 @@ from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
 import requests
 
+
+def SpecificElementFinder(soup: BeautifulSoup, element, substring) -> None:
+
+    all_specific_elements = soup.find_all(
+        f"{element}", string=lambda text: f"{substring}" in text.lower()
+    )
+
+    # # Still specific to this particular site...
+    # specific_elements = [
+    #     el.parent.parent.parent for el in all_specific_elements
+    # ]
+
+    # print(f"Found: {len(all_specific_elements)} of those jobs")
+
+    # # Also specific to this site
+    # for job_element in specific_elements:
+    #     link_url = job_element.find_all("a")[1]["href"]
+    #     print(f"Apply here: {link_url}\n")
+
 # Testing child retrieval
 
 
