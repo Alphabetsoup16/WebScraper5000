8da88c4dd47fee6311d02b646244c1ea32d0af3d
LoneHippie <lonehippie.music@gmail.com>
2022-02-22 03:39:07 +0200

    organized api.py, added beginning of gui and set up frontend with the api

--- source/api.py
+++ source/api.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI, Request
-from starlette.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 import uvicorn
 from utilities import UseConfig
-import json
 
-app = FastAPI()
+# later this should be a .env value or || 8000
+PORT = 8000
+# might need to change later for deployment
+HOST = '127.0.0.1'
+TITLE = 'Web Scraper of Doom'
+API_URL = "/api/v1/"
 
+app = FastAPI(
+    title=TITLE,
+    openapi_url=API_URL
+)
+
+app.mount("/static", StaticFiles(directory='static'))
 
 @app.get("/")
-async def main():
-    return RedirectResponse(url="/docs/")
+async def serve_static():
+    return FileResponse('static/index.html', media_type='text/html')
+@app.get("/script.js")
+async def load_js():
+    return FileResponse('static/script.js')
 
-@app.post("/scrape")
+
+@app.post(f"{API_URL}/scrape")
 async def get_config(request: Request):
     config = await request.json()
     response = {}
@@ -20,4 +35,4 @@ async def get_config(request: Request):
 
 
 if __name__ == '__main__':
-    uvicorn.run("api:app", reload=True, port=8000, host='127.0.0.1')
+    uvicorn.run("api:app", reload=True, port=PORT, host=HOST)
