9ce145e9b6dcca8492ac0fd0ae945f766fb3eb28
LoneHippie <lonehippie.music@gmail.com>
2022-02-22 06:52:08 +0200

    working interactive ui

--- source/static/script.js
+++ source/static/script.js
@@ -1,39 +1,50 @@
 const DOM = {
     form: document.getElementById('config-form'),
+    configGenPoint: document.getElementById('config-gen'),
     addConfigBtn: document.getElementById('add-config')
 };
 
 const handleSubmit = (event) => {
     console.log('submit triggered');
     event.preventDefault();
-}
+};
 
 DOM.form.onsubmit = handleSubmit;
 
-const newConfigHtml = `
-    <div class="config">
-        <div class="config__group">
-            <input
-                type="text"
-                name="config-name"
-            ><label for="name-config">Name</label>
-        </div>
-        <div class="config__group">
-            <input
-                type="text"
-                name="config-method"
-            ><label for="method-config">Method</label>
-        </div>
-        <div class="config__group">
-            <input
-                type="text"
-                name="config-arguments"
-            ><label for="arguments-config">Arguments</label>
-        </div>
-    </div>
-`;
+let idCount = 0;
+
+const newConfigHtml = (count) => {
+    return (
+        `
+            <div class="config" id="config-item-${count}">
+                <div class="config__group">
+                    <input
+                        type="text"
+                        name="config-name"
+                        id="config-name-${count}"
+                    ><label for="name-config">Name</label>
+                </div>
+                <div class="config__group">
+                    <input
+                        type="text"
+                        name="config-method"
+                        id="config-method-${count}"
+                    ><label for="method-config">Method</label>
+                </div>
+                <div class="config__group">
+                    <input
+                        type="text"
+                        name="config-arguments"
+                        id="config-arguments-${count}"
+                    ><label for="arguments-config">Arguments</label>
+                </div>
+            </div>
+        `
+    )
+}
 
 DOM.addConfigBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    DOM.form.insertAdjacentHTML('afterbegin', newConfigHtml)
+    DOM.configGenPoint.insertAdjacentHTML('beforeend', newConfigHtml(idCount))
+    idCount++;
 })
