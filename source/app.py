fe7ec86da138dccb6f0a5754794752101e678795
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-02-22 22:23:48 -0500

    WOW lol

--- source/app.py
+++ source/app.py
@@ -1,6 +1,4 @@
-from ast import Num
-from typing import Dict, List
-from attr import attrs
+from typing import List
 from bs4 import BeautifulSoup
 
 from utilities.General_Utilities import GetMeTheSoup
@@ -8,52 +6,82 @@ from utilities.General_Utilities import GetMeTheSoup
 
 # fake static job site for testing: https://realpython.github.io/fake-jobs/
 
+# def GetElementByAttribute2(soup: BeautifulSoup,  Attrs: List):
+#     data = []
+#     for dict in Attrs:
+#         specific_element = soup.find_all(attrs=dict)
+#         labels = dict.values()
+#         label = list(labels)[0]
+#         for target in specific_element:
+#             print(f"{label}: {target.get_text().strip()}")
+#     #         data.append(f"{label}: {target.get_text().strip()}")
+#     # return data
 
-def GetElementByAttribute(soup: BeautifulSoup,  Attrs: List):
-    for a in range(len(Attrs)):
-        specific_element = soup.find_all(attrs=Attrs[a])
-        label = Attrs[a].values()
-        for attr in specific_element:
-            print(f"{list(label)[0]}: {attr.get_text().strip()}")
+# def GetElementByAttribute(soup: BeautifulSoup,  Attrs: list):
+#     for a in range(len(Attrs)):
+#         specific_element = soup.find_all(attrs=Attrs[a])
+#         label = Attrs[a].values()
+#         for attr in specific_element:
+#             print(f"{list(label)[0]}: {attr.get_text().strip()}")
 
 
-def GetElementByAttribute3(soup: BeautifulSoup,  Attrs: List):
-    data = []
+# def GetElementByAttribute3(soup: BeautifulSoup,  Attrs: list):
+#     for dict in Attrs:
+#         specific_element = soup.find_all(attrs=dict)
+#         labels = dict.values()
+#         label = list(labels)[0]
+#         ElementBuilder(specific_element, label)
+
+
+def GetElementByAttribute4(soup: BeautifulSoup,  Attrs: list):
+    all_specific_elements = []
+
+    for dict in Attrs:
+
+        specific_element = soup.find_all(attrs=dict)
+        all_specific_elements.append(specific_element)
+
+    return all_specific_elements
+
+
+def ElementBuilder(elementLists, all_attributes):
+    targetElements = []
     targets = {}
-    for dict in Attrs:
-        specific_element = soup.find_all(attrs=dict)
-        labels = dict.values()
-        label = list(labels)[0]
-        for target in specific_element:
+    n = 0
+    for elements in elementLists:
+        for target in elements:
             targets = {
-                "Id": specific_element.index(target),
-                label: target.get_text().strip()
+                "Id": elements.index(target),
+                all_attributes[n]: target.get_text().strip()
             }
-            data.append(targets)
-    return data
+            targetElements.append(targets)
+        n += 1
+    return targetElements
 
 
-def GetElementByAttribute2(soup: BeautifulSoup,  Attrs: List):
-    data = []
+def ResultHandler(extractedResult: list):
+    return
+
+
+def AttributeHandler(Attrs: list):
+    all_attributes = []
     for dict in Attrs:
-        specific_element = soup.find_all(attrs=dict)
-        labels = dict.values()
-        label = list(labels)[0]
-        for target in specific_element:
-            print(f"{label}: {target.get_text().strip()}")
-    #         data.append(f"{label}: {target.get_text().strip()}")
-    # return data
+        all_attributes.append("".join(dict.values()))
+    return all_attributes
 
 
 def main() -> None:
 
     soup = GetMeTheSoup("https://realpython.github.io/fake-jobs/")
 
-    # print(GetElementByAttribute(soup, [
-    #       {"class": "title"}, {"class": "location"}]))
+    attributes = [{"class": "title is-5"}, {"class": "location"}]
 
-    print(GetElementByAttribute2(soup, [
-          {"class": "title is-5"}, {"class": "location"}]))
+    all_attributes = AttributeHandler(attributes)
+
+    targetedAttributes = GetElementByAttribute4(soup, attributes)
+
+    targetElements = ElementBuilder(targetedAttributes, all_attributes)
+    print(targetElements)
 
 
 if __name__ == "__main__":
