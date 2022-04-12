7ef9077b1004f04b1e525c59e00b241fe10b61a5
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-04-11 21:42:38 -0400

    clean up

--- source/BETA_app.py
+++ source/BETA_app.py
@@ -1,4 +1,5 @@
 import json
+import re
 from bs4 import BeautifulSoup
 from utilities.general_utilities import GetMeTheSoup
 
@@ -102,3 +103,29 @@ def AggregateDuplicateAttributes(config_attr: list):
                 attr_value_list.append({key: attr_val})
         final_attr_list.append(attr_value_list)
     return final_attr_list
+
+
+def ElementsWithRegexByClass(soup: BeautifulSoup, class_string: str):
+    return soup.find_all(class_=re.compile(class_string))
+
+
+def ElementsWithRegexById(soup: BeautifulSoup, id_string: str):
+    return soup.find_all(id=re.compile(id_string))
+
+
+def ElementsWithRegexByString(soup: BeautifulSoup, string: str):
+    return soup.find_all(string=re.compile(string))
+
+
+def GetElementWithRegex(json_data: dict, regex_search: str, regex_type: str):
+    # Need to test this more
+    soup: BeautifulSoup = GetMeTheSoup(json_data["url"])
+    if regex_type == "class":
+        return ElementsWithRegexByClass(soup, regex_search)
+    elif regex_type == "string":
+        return ElementsWithRegexByString(soup, regex_search)
+    elif regex_type == "id":
+        return ElementsWithRegexById(soup, regex_search)
+    else:
+        print("you can only assign regex_type to class, string, or id")
+        return
--- source/TEST_app.py
+++ source/TEST_app.py
@@ -6,33 +6,7 @@ from utilities.general_utilities import GetMeTheSoup, GetHeaderInfo, SaveAsJson
 # fake static job site for testing
 URL = "https://realpython.github.io/fake-jobs/"
 
-
-def ElementsWithRegexByClass(soup: BeautifulSoup, class_string: str):
-    return soup.find_all(class_=re.compile(class_string))
-
-
-def ElementsWithRegexById(soup: BeautifulSoup, id_string: str):
-    return soup.find_all(id=re.compile(id_string))
-
-
-def ElementsWithRegexByString(soup: BeautifulSoup, string: str):
-    return soup.find_all(string=re.compile(string))
-
-
-def GetElementWithRegex(json_data: dict, regex_search: str, regex_type: str):
-    # Need to test this
-    soup: BeautifulSoup = GetMeTheSoup(json_data["url"])
-    if regex_type == "class":
-        return ElementsWithRegexByClass(soup, regex_search)
-    elif regex_type == "string":
-        return ElementsWithRegexByString(soup, regex_search)
-    elif regex_type == "id":
-        return ElementsWithRegexById(soup, regex_search)
-    else:
-        print("you can only assign regex_type to class, string, or id")
-        return
-
-        #############################---Functions above need to be tested---#############################
+#############################---Functions above need to be tested---#############################
 
 
 def main() -> None:
